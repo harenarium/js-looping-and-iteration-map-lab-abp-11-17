c23237588fbd1805fa38f375e76a959b7b66d638
harenarium <carolinel2610@gmail.com>
2017-11-10 21:44:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers){
+  return drivers.map(function(name) {return name.toLowerCase()})
+}
+
+
+function nameToAttributes(drivers){
+  const newArr = []
+  for(const fullname of drivers){
+    let spaceSwitch = true
+    let firstName = ""
+    let lastName = ""
+    for (const char of fullname){
+      if (char === " "){
+        spaceSwitch = false
+        continue
+      }
+      spaceSwitch ? firstName = firstName + char : lastName = lastName + char
+    }
+    newArr.push({'firstName':firstName,'lastName':lastName})
+  }
+  return newArr
+}
+
+function attributesToPhrase(drivers){
+  return drivers.map(function(driver) {return `${driver.name} is from ${driver.hometown}`})
+}
